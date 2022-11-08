e3f2dcf08dbb49c2bf1e79e72f695b5e2052ae6b
Thomas Bittermann <pufferbrater@cobonet.de>
2022-11-08 20:47:48 +0100

    content missing

--- mw/src/main/java/team05/mw/MwConfig.java
+++ mw/src/main/java/team05/mw/MwConfig.java
@@ -1,2 +1,12 @@
-package team05.mw;public class MwConfig {
+package team05.mw;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithConverter;
+import team05.mw.smallrye.ConfigReadable;
+import team05.mw.smallrye.ConfigReadableConverter;
+
+@ConfigMapping(prefix = "team05.mw")
+public interface MwConfig {
+    @WithConverter(ConfigReadableConverter.class)
+    ConfigReadable raststaetten();
 }
--- mw/src/main/java/team05/mw/geo/GeoService.java
+++ mw/src/main/java/team05/mw/geo/GeoService.java
@@ -1,2 +1,40 @@
-package team05.mw.geo;public class GeoService {
+package team05.mw.geo;
+
+import io.quarkus.runtime.Startup;
+import team05.mw.MwConfig;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+@ApplicationScoped
+@Startup
+public class GeoService {
+
+    private final MwConfig config;
+
+    public GeoService(MwConfig config) {
+        this.config = config;
+    }
+
+    @PostConstruct
+    public void setup() {
+        StringBuilder textBuilder = new StringBuilder();
+        try (Reader reader = new BufferedReader(new InputStreamReader
+                (config.raststaetten().asInputStream(), Charset.forName(StandardCharsets.UTF_8.name())))) {
+            int c = 0;
+            while ((c = reader.read()) != -1) {
+                textBuilder.append((char) c);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String json = textBuilder.toString();
+    }
+
 }
