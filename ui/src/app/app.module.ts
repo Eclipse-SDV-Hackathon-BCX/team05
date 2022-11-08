5b7ab054bea764a796b5f8c66e154a0ec463bc18
andreas pross <anpross@gmail.com>
2022-11-08 21:46:13 +0100

    UI: more panel changes

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IconModule, UIShellModule } from 'carbon-components-angular';
+import { ButtonModule } from 'carbon-components-angular';
 
 import { AppComponent } from './app.component';
 import { BreakComponent } from './break/break.component';
+import { BadgeComponent } from './common/badge/badge.component';
+import { IconComponent } from './common/icon/icon.component';
 import { DestinationComponent } from './destination/destination.component';
 import { MapComponent } from './map/map.component';
 import { PanelComponent } from './panel/panel.component';
 import { RecommendationComponent } from './recommendation/recommendation.component';
-import { IconComponent } from './common/icon/icon.component';
-
 
 @NgModule({
   declarations: [
@@ -20,9 +20,10 @@ import { IconComponent } from './common/icon/icon.component';
     RecommendationComponent,
     BreakComponent,
     IconComponent,
+    BadgeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
