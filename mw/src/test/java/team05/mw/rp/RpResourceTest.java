07943709172531ae3b03760fe4d9f3e541b65d7c
Thomas Bittermann <pufferbrater@cobonet.de>
2022-11-08 14:17:45 +0100

    fixed tests

---
+++ mw/src/test/java/team05/mw/rp/RpResourceTest.java
@@ -0,0 +1,30 @@
+package team05.mw.rp;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class RpResourceTest {
+
+    @Test
+    void list() {
+        given()
+                .when().get("/rp")
+                .then()
+                .statusCode(200)
+                .body(is("[{\"id\":\"RP-1\",\"name\":\"Heide Nord\",\"full\":false},{\"id\":\"RP-2\",\"name\":\"Zum Wilden Hirsch\",\"full\":true}]"));
+    }
+
+    @Test
+    void getRp() {
+        given()
+                .when().get("/rp/RP-1")
+                .then()
+                .statusCode(200)
+                .body(is("{\"id\":\"RP-1\",\"name\":\"Heide Nord\",\"coordinates\":{\"lat\":0.0,\"lng\":0.0},\"capacity\":3,\"occupancy\":2}"));
+    }
+}
