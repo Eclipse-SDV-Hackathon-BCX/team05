720f2f193f0aaac447f82e8fcd02ae0512f4c335
andreas pross <anpross@gmail.com>
2022-11-08 17:59:50 +0100

    UI: more ui stuff

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ui';
+
+  ngOnInit(): void {
+  }
 }
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IconModule, UIShellModule } from 'carbon-components-angular';
 
 import { AppComponent } from './app.component';
+import { BreakComponent } from './break/break.component';
+import { DestinationComponent } from './destination/destination.component';
 import { MapComponent } from './map/map.component';
+import { PanelComponent } from './panel/panel.component';
+import { RecommendationComponent } from './recommendation/recommendation.component';
+import { IconComponent } from './common/icon/icon.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    PanelComponent,
+    DestinationComponent,
+    RecommendationComponent,
+    BreakComponent,
+    IconComponent,
   ],
   imports: [
     BrowserModule
