9c808cd7c61ced810431f3e392de5d9e2225fd6c
andreas pross <anpross@gmail.com>
2022-11-08 18:01:03 +0100

    UI: more ui stuff

---
+++ ui/src/app/destination/destination.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { IconService } from "carbon-components-angular";
+
+@Component({
+  selector: 'app-destination',
+  templateUrl: './destination.component.html',
+  styleUrls: ['./destination.component.css']
+})
+export class DestinationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
