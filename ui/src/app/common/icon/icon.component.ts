15e0842ce056bca8b50a95c4553dde167609cf66
andreas pross <anpross@gmail.com>
2022-11-09 09:12:50 +0100

    UI: change to scss

--- ui/src/app/common/icon/icon.component.ts
+++ ui/src/app/common/icon/icon.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.css']
+  styleUrls: ['./icon.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class IconComponent implements OnInit {
 
   @Input() icon: string = '';
   @Input() color: string = '';
+  @Input() customCss: string = '';
 
   constructor() { }
 
