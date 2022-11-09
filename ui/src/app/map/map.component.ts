7ddc3a5903f8ff36e8e67130a92942d13039d44f
Mathis Moder <mathis.moder@gmail.com>
2022-11-09 11:01:37 +0100

    get markers from backend

--- ui/src/app/map/map.component.ts
+++ ui/src/app/map/map.component.ts
@@ -1,6 +1,11 @@
 import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
 import * as L from 'leaflet';
-import { ServiceStationService } from '../service-station.service';
+import { ServiceStationService, Backend_Service_model } from '../service-station.service';
+
+interface UI_Station{
+  station: Backend_Service_model,
+  marker: L.Marker
+}
 
 @Component({
   selector: 'app-map',
@@ -12,46 +17,72 @@ export class MapComponent implements AfterViewInit {
 
   private map:any;
 
+  private stations:UI_Station[] = [];
+
+  private timeMarker = L.icon({
+    iconUrl: 'assets/time_black.svg',
+    iconSize:     [80, 90], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  })
+
+  private stationMarker = L.icon({
+    iconUrl: 'assets/fuel_green.svg',
+    iconSize:     [80, 90], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  })
+
+  constructor(private stationService: ServiceStationService) { 
+  }
+
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 52.68, 13.02 ],
-      zoom: 13,
-      zoomControl: false,
+      center: [ 52.68, 13.05 ],
+      zoom: 10,
+      zoomControl: true,
       attributionControl: false,
-      dragging: false
+      dragging: true
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 13,
+      maxZoom: 10,
       minZoom: 13,
 
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
-    var timeMarker = L.icon({
-      iconUrl: 'assets/time_black.svg',
-  
-      iconSize:     [80, 90], // size of the icon
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    })
-
-    L.marker([52.708480, 13.0232002], {icon: timeMarker}).addTo(this.map);
-
-    L.marker([52.6651948, 13.0032002]).addTo(this.map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    L.marker([52.708480, 13.0232002], {icon: this.timeMarker}).addTo(this.map);
 
     tiles.addTo(this.map);
   }
 
-  constructor(private stationService: ServiceStationService) { 
+  addStations(stations:Backend_Service_model[]):void {
+    stations.forEach(station => {
+      let marker = L.marker([station.coordinates.lng, station.coordinates.lat], {icon: this.stationMarker})
+        .addTo(this.map)
+        .bindPopup(station.name)
+        this.stations.push({station, marker});
+        
+    })
+  }
 
+  private updateStationOccupancy():void {
+    this.stations.forEach(stat => {
+   //   console.log(stat);
+   //   let status = this.stationService.getStatusfromOcupancy(stat.station.capacity, s.occupancy)
+      stat.marker.setIcon(this.timeMarker);
+    });
   }
 
   ngAfterViewInit(): void {
     this.initMap();
-    this.stationService.getStations()
+    this.stationService.getStations().subscribe(
+      stations => this.addStations(stations)
+
+    )
+
+    setTimeout(() => { this.updateStationOccupancy() }, 5000);
   }
 
 }
--- ui/src/app/service-station.service.ts
+++ ui/src/app/service-station.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http"
-import { switchMap } from 'rxjs';
+import { Observable, Subscription, switchMap } from 'rxjs';
 
 
 enum Occupancy {
@@ -9,13 +9,13 @@ enum Occupancy {
   Free
 }
 
-interface Service_Station {
+export interface Service_Station {
   latitude: number
   longditude: number
   status: Occupancy
 }
 
-interface Backend_Service_model{
+export interface Backend_Service_model{
   id: string,
   name: string,
   capacity: number,
@@ -35,32 +35,11 @@ export class ServiceStationService {
 
   getStatusfromOcupancy(capacity: number, occupancy: number): Occupancy {
     if(occupancy >= capacity) return Occupancy.Full
-    else if( occupancy > (capacity/2)) return Occupancy.Medium
     else return Occupancy.Free
   }
 
-  getStations(): Service_Station[]{
-    if (this.service_stations.length > 0)
-    {return this.service_stations}
-    else {
-      this.http.get<Backend_Service_model[]>('/api/station/list')
-      .subscribe((result: Backend_Service_model[])=>{
-        console.log(result)
-        this.service_stations = result.map((service_station)=>{
-          let station_object: Service_Station = {
-            latitude: service_station.coordinates.lat,
-            longditude: service_station.coordinates.lng,
-            status: this.getStatusfromOcupancy(service_station.capacity, service_station.occupancy)
-          }
-          return station_object
-        })
-        console.log(this.service_stations)
-        return this.service_stations
-      })
-    }
-    return []
+  getStations(): Observable<Backend_Service_model[]>{
+      return this.http.get<Backend_Service_model[]>('/api/station/list')
   }
-  
-
 
 }
