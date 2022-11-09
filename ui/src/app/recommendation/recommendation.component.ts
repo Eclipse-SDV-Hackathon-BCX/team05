182a360e48fd33a1fdee4a55fc9558eddc8aa01d
andreas pross <anpross@gmail.com>
2022-11-09 11:57:09 +0100

    UI: stuff and things

--- ui/src/app/recommendation/recommendation.component.ts
+++ ui/src/app/recommendation/recommendation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ServiceStationService } from '../service-station.service';
+import { Backend_Service_model, Service_Station, ServiceStationService } from '../service-station.service';
 
 @Component({
   selector: 'app-recommendation',
@@ -8,11 +8,23 @@ import { ServiceStationService } from '../service-station.service';
 })
 export class RecommendationComponent implements OnInit {
 
+  recommendedStation?: Backend_Service_model;
+
   constructor(private stationService: ServiceStationService) {
   }
 
   ngOnInit(): void {
+    setTimeout(() => this.updateStation(), 5000);
+  }
 
+  updateStation() {
+    this.stationService.getStations().subscribe(
+      stations => {
+        this.recommendedStation = stations.filter(station => {
+          return station.capacity >= station.occupancy;
+        }).pop()
+      }
+    )
   }
 
 }
