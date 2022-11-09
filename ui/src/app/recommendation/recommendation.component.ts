3bc9b0ee4d4c6fc50ac71218092baecc0e7396ac
andreas pross <anpross@gmail.com>
2022-11-09 12:31:04 +0100

    UI: stuff and things

--- ui/src/app/recommendation/recommendation.component.ts
+++ ui/src/app/recommendation/recommendation.component.ts
@@ -10,6 +10,12 @@ export class RecommendationComponent implements OnInit {
 
   recommendedStation?: Backend_Service_model;
 
+  stationData = [
+    '11km | 00:05',
+    '23km | 00:12',
+    '49km | 00:30'
+  ];
+
   constructor(private stationService: ServiceStationService) {
   }
 
@@ -20,11 +26,25 @@ export class RecommendationComponent implements OnInit {
   updateStation() {
     this.stationService.getStations().subscribe(
       stations => {
-        this.recommendedStation = stations.filter(station => {
+        let newStation = stations.filter(station => {
           return station.capacity >= station.occupancy;
-        }).pop()
+        }).pop();
+        if (this.recommendedStation?.id != newStation?.id) {
+
+        }
+        this.recommendedStation = newStation;
       }
-    )
+    );
+  }
+
+  getParkingText(): string {
+    let currStationId: number = parseInt((this.recommendedStation?.id.substr(3, 1) || '4'));
+
+    return this.stationData[currStationId];
+  }
+
+  getParkingStyle() {
+
   }
 
 }
